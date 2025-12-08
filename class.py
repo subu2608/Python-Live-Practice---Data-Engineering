b44fbd028ded0ad1803eb80672e0cff07a1ccad8
subu2608 <147263810+subu2608@users.noreply.github.com>
2025-12-08 22:46:25 +0530

    Updated

---
+++ class.py
@@ -0,0 +1,12 @@
+class Subu:
+    def frnds(self):
+        print("Lot of friends")
+s= Subu()
+s.frnds()
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+p1 = Person("Arun")
+print(p1.name)
